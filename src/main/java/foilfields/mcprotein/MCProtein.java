d81bc6602bf0ddb331a98b20cc9751caa7172495
woukie <lucaseldon@live.com>
2023-01-30 16:39:36 +0000

    Initial Commit

---
+++ src/main/java/foilfields/mcprotein/MCProtein.java
@@ -0,0 +1,10 @@
+package foilfields.mcprotein;
+
+import net.fabricmc.api.ModInitializer;
+
+public class MCProtein implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
