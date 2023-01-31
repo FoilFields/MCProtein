fc3e03f925106d78339f0b3aa8b2ae7b5cd3a3b5
woukie <lucaseldon@live.com>
2023-01-31 10:28:44 +0000

    Changed namespaces in identifiers to use global variable

--- src/main/java/foilfields/mcprotein/MCProtein.java
+++ src/main/java/foilfields/mcprotein/MCProtein.java
@@ -20,19 +20,19 @@ public class MCProtein implements ModInitializer {
     public static FlowableFluid FLOWING_FISH_OIL;
     public static Item FISH_OIL_BUCKET, POWDERED_MILK;
     public static Block FISH_OIL;
+    public static String MOD_ID = "mcprotein";
     //custom item group
-    final ItemGroup MCPROTEIN = FabricItemGroupBuilder.create(new Identifier("mcprotein", "mcprotein")).icon(() -> new ItemStack(FISH_OIL_BUCKET)).build();
-
+    final ItemGroup MCPROTEIN = FabricItemGroupBuilder.create(new Identifier(MOD_ID, "mcprotein")).icon(() -> new ItemStack(FISH_OIL_BUCKET)).build();
 
     @Override
     public void onInitialize() {
-        STILL_FISH_OIL = Registry.register(Registry.FLUID, new Identifier("mcprotein", "fish_oil"), new FishOilFluid.Still());
-        FLOWING_FISH_OIL = Registry.register(Registry.FLUID, new Identifier("mcprotein", "flowing_fish_oil"), new FishOilFluid.Flowing());
-        FISH_OIL_BUCKET = Registry.register(Registry.ITEM, new Identifier("mcprotein", "fish_oil_bucket"),
+        STILL_FISH_OIL = Registry.register(Registry.FLUID, new Identifier(MOD_ID, "fish_oil"), new FishOilFluid.Still());
+        FLOWING_FISH_OIL = Registry.register(Registry.FLUID, new Identifier(MOD_ID, "flowing_fish_oil"), new FishOilFluid.Flowing());
+        FISH_OIL_BUCKET = Registry.register(Registry.ITEM, new Identifier(MOD_ID, "fish_oil_bucket"),
         new BucketItem(STILL_FISH_OIL, new Item.Settings().recipeRemainder(Items.BUCKET).maxCount(1).group(MCPROTEIN)));
-        FISH_OIL = Registry.register(Registry.BLOCK, new Identifier("mcprotein", "fish_oil"), new FluidBlock(STILL_FISH_OIL, FabricBlockSettings.copy(Blocks.WATER)){});
+        FISH_OIL = Registry.register(Registry.BLOCK, new Identifier(MOD_ID, "fish_oil"), new FluidBlock(STILL_FISH_OIL, FabricBlockSettings.copy(Blocks.WATER)){});
 
-        POWDERED_MILK = Registry.register(Registry.ITEM, new Identifier("mcprotein", "powdered_milk"), new Item(new FabricItemSettings().group(MCPROTEIN)));
+        POWDERED_MILK = Registry.register(Registry.ITEM, new Identifier(MOD_ID, "powdered_milk"), new Item(new FabricItemSettings().group(MCPROTEIN)));
     }
 
 }
