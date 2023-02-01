03f2417ba4071afbd46e92c6061250a3d9848023
woukie <lucaseldon@live.com>
2023-02-01 20:57:15 +0000

    Added curd and cheese items and textures

---
+++ src/main/java/foilfields/mcprotein/block/CurdBlock.java
@@ -0,0 +1,19 @@
+package foilfields.mcprotein.block;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.ShapeContext;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.util.shape.VoxelShapes;
+
+public class CurdBlock extends Block {
+    public CurdBlock(Settings settings) {
+        super(settings);
+    }
+
+    @Override
+    public VoxelShape getOutlineShape(BlockState state, net.minecraft.world.BlockView world, BlockPos pos, ShapeContext context) {
+        return VoxelShapes.cuboid(0f, 0f, 0f, 1f, 0.0625f, 1f);
+    }
+}
